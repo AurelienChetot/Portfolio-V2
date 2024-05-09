9f8b3e7bbfba843b2cb7663264b5f9c93d0aa73c
AurelienChetot <aurelien.chetot@gmail.com>
2024-05-09 13:19:35 +0200

    Add Background and init projet page

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -1,21 +1,28 @@
 import "../App.scss";
 
+import VIDEO from "../videos/background.mp4";
+
 function Projet() {
   return (
     <>
-      <h1>Projet 1 Qui?Zip :</h1>
-      <div className="text-container">
-        <p>
-          Pour mon projet 1, j'ai collaboré avec deux autres personnes sur la
-          création d'un Quiz nommé Qui?Zip. Notre objectif principal était de le
-          développer dans un délai imparti tout en assurant sa fonctionnalité.
-          En utilisant les langages HTML, CSS et JavaScript Vanilla, nous avons
-          travaillé ensemble pour concevoir une interface conviviale et
-          intuitive. Cette collaboration nous a permis de mettre en pratique nos
-          compétences en développement web et de créer un produit final dont
-          nous sommes fiers.
-        </p>
-      </div>
+      <body>
+        <video className="background-video" src={VIDEO} autoPlay muted loop />
+        <div className="projet-container">
+          <h1>Projet 1 Qui?Zip :</h1>
+          <div className="text-container">
+            <p>
+              Pour mon projet 1, j'ai collaboré avec deux autres personnes sur
+              la création d'un Quiz nommé Qui?Zip. Notre objectif principal
+              était de le développer dans un délai imparti tout en assurant sa
+              fonctionnalité. En utilisant les langages HTML, CSS et JavaScript
+              Vanilla, nous avons travaillé ensemble pour concevoir une
+              interface conviviale et intuitive. Cette collaboration nous a
+              permis de mettre en pratique nos compétences en développement web
+              et de créer un produit final dont nous sommes fiers.
+            </p>
+          </div>
+        </div>
+      </body>
     </>
   );
 }
