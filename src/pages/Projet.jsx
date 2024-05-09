8796fbb9b96c3773f93569ba415d71863946ea53
AurelienChetot <aurelien.chetot@gmail.com>
2024-05-09 17:42:15 +0200

    add Projet

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -1,4 +1,7 @@
 import "../App.scss";
+import PROJET1 from "../images/projet1.gif";
+import PROJET2 from "../images/projet2.gif";
+import COMINGSOON from "../images/comingsoon.gif";
 
 import VIDEO from "../videos/background2.mp4";
 
@@ -7,10 +10,14 @@ function Projet() {
     <>
       <body>
         <video className="background-video" src={VIDEO} autoPlay muted loop />
-        <div className="projet-container">
-          <h1>Projet 1 Qui?Zip :</h1>
-          <div className="text-container">
-            <p>
+        <h1>Mes Projets :</h1>
+        <div className="">
+          <div className="projet-container">
+            <h1>Projet 1 Qui?Zip :</h1>
+            <div className="img-container">
+              <img src={PROJET1} alt="projet1" />
+            </div>
+            <p className="text-projet">
               Pour mon projet 1, j'ai collaboré avec deux autres personnes sur
               la création d'un Quiz nommé Qui?Zip. Notre objectif principal
               était de le développer dans un délai imparti tout en assurant sa
@@ -19,6 +26,52 @@ function Projet() {
               interface conviviale et intuitive. Cette collaboration nous a
               permis de mettre en pratique nos compétences en développement web
               et de créer un produit final dont nous sommes fiers.
+              <br />{" "}
+              <span className="style-text-projet">Retrouvez le Quiz sur :</span>
+              <br />
+              <a
+                href="https://aurelienchetot.github.io/Projet-1-Team-3/"
+                target="_blank"
+              >
+                Qui?Zip
+              </a>
+            </p>
+          </div>
+          <div className="projet-container">
+            <h2>Projet 2 Star-Wild :</h2>
+            <div className="img-container">
+              <img src={PROJET2} alt="projet2" />
+            </div>
+            <p className="text-projet">
+              Pour notre projet Star-Wild, une équipe de quatre personnes a
+              relevé le défi de créer une expérience innovante. Avec React.js,
+              Node.js et ThreeJS, nous avons développé une visite du système
+              solaire. Notre collaboration nous a permis de livrer un produit
+              dont nous sommes fiers, tout en renforçant nos compétences en
+              développement web.
+              <br />{" "}
+              <span className="style-text-projet">
+                Retrouvez Star-Wild sur :
+              </span>
+              <br />
+              <a href="https://star-wild.netlify.app/" target="_blank">
+                Star-Wild
+              </a>
+            </p>
+          </div>
+          <div className="projet-container">
+            <h2>Coming Soon :</h2>
+            <div className="img-container">
+              <img src={COMINGSOON} alt="coming soon" />
+            </div>
+            <p className="text-projet">
+              Actuellement en formation, je suis impliqué dans le développement
+              de plusieurs projets passionnants qui sont en cours de
+              réalisation. Ces projets représentent une opportunité unique
+              d'appliquer les connaissances acquises et de perfectionner mes
+              compétences dans divers domaines. Je suis enthousiaste à l'idée de
+              voir ces projets évoluer.
+              <br /> <span className="style-text-projet">Aurélien Chetot</span>
             </p>
           </div>
         </div>
